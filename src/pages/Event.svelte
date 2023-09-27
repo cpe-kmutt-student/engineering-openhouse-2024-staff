5325dcf260a86d2b0c0ad26a634d7ba6abb26228
SSSBoOm <boom1909chanapat@gmail.com>
2023-09-28 02:25:29 +0700

    feat: get qrText after genQR

--- src/pages/Event.svelte
+++ src/pages/Event.svelte
@@ -9,27 +9,34 @@
   let qrText = ""
   const frontUrl = import.meta.env.VITE_FRONTEND_URL || "http://localhost:3000"
 
+  const getQrcode = async () => {
+    const res = await Axios.post("/api/qrcode", { event_id: id })
+    if (res.data.data) {
+      qrText = res.data.data.id || ""
+    }
+  }
+
   onMount(async () => {
     if (!$User) {
       window.location.pathname = "/"
     }
-    const res = await Axios.post("/api/qrcode", { event_id: id })
-    if (res.data.data) {
-      qrText = res.data.data.id
-    }
-    console.log(res.data.data)
+    getQrcode()
   })
   // TODO : Check interval to check expired
   const intervalCheckExpired = () => {}
 
   const genQR = async () => {
-    await Axios.post("/api/qrcode/create", { event_id: id })
+    const res = await Axios.post("/api/qrcode/create", { event_id: id })
+
+    await getQrcode()
   }
 </script>
 
 <div>
   <div class="flex justify-center">
-    <button on:click={genQR} class="btn-dark"> สร้าง Qr Code </button>
+    {#if qrText == ""}
+      <button on:click={genQR} class="btn-dark"> สร้าง Qr Code </button>
+    {/if}
   </div>
   <h1>{qrText}</h1>
   {#if qrText}
